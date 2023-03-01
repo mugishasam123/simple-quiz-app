c388ef72ee813b86955eae72441c1b6f8c151569
mugishasam123 <mugishasamuel42@gmail.com>
2023-03-01 16:18:52 +0200

    Add tests for components

---
+++ src/__tests__/components.test.jsx
@@ -0,0 +1,34 @@
+import { describe, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import QuestionCard from "../components/QuestionCard/QuestionCard.jsx";
+import QuestionList from "../components/QuestionList/QuestionList.jsx";
+import Questions from "../Data/Questions.jsx";
+
+describe("Accordion test", () => {
+  test("testing if QuestionCard is rendered", () => {
+    const option = { id: 0, text: "New York City", isCorrect: false };
+    const optionClicked = () => {
+      console.log("this is questioncard");
+    };
+
+    expect(
+      render(<QuestionCard option={option} optionClicked={optionClicked} />)
+    ).toMatchSnapshot();
+  });
+
+  test("if QuestionList renders correctly", () => {
+    const currentQuestion = 1;
+    const optionClicked = () => {
+      console.log("this is questionlist");
+    };
+    expect(
+      render(
+        <QuestionList
+          optionClicked={optionClicked}
+          Questions={Questions}
+          currentQuestion={currentQuestion}
+        />
+      )
+    ).toMatchSnapshot();
+  });
+});
